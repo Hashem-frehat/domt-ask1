313050eb3927ff80d1c20e803de56b96a164c77c
hashem-frehat <hashem.abofreh@gmail.com>
2024-06-09 21:59:40 +0300

    this is the dom-task1

---
+++ app.js
@@ -0,0 +1,38 @@
+
+// -----------------------------------------q1
+
+let fruits =[
+"apple.webp"
+,"orange.jpg"
+,"banana.webp"
+,"watermelon.webp",
+"blueberry.jpg"];
+let fruitsul=document.getElementById("fruitsul");
+
+fruits.forEach(a => {
+    
+    let li1 = document.createElement("li");
+    let imgg =document.createElement("img");
+       imgg.src = a;
+       imgg.style.width = "100px";
+       imgg.style.height="100px"
+       li1.style.listStyle ="none";
+       li1.appendChild(imgg);
+       fruitsul.appendChild(li1);
+ 
+    
+});
+
+
+// --------------------------------------------q2
+let myarray = [1,2,3,4,5,6,7,8,9,10];
+function checker(arrr, num){
+for(i=0 ; i<=myarray.length ; i++){
+    if(arrr[i]===num){
+        return console.log(i) ;
+    }
+    
+}
+return console.log("-1");
+}
+checker(myarray,5);
